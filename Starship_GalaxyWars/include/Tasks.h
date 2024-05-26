b4593e94bfe343691b2cb4650414e017f63611b2
Salads <dgentry.email@protonmail.com>
2024-05-26 05:34:37 -0700

    Set all tasks enum size to 8 bit (smallest), to preserve memory usage.

--- Starship_GalaxyWars/include/Tasks.h
+++ Starship_GalaxyWars/include/Tasks.h
@@ -6,11 +6,11 @@
 #define NUM_TASKS 1
 #define PERIOD_GCD 1000
 
-enum TS_Joystick {TS_JOYSTICK_INIT, TS_JOYSTICK_SAMPLE};
-enum TS_Buttons  {TS_BUTTONS_INIT,  TS_BUTTONS_SAMPLE };
-enum TS_Update 	 {TS_UPDATE_INIT,   TS_UPDATE_DOUPDATE};
-enum TS_Audio    {TS_AUDIO_INIT,    TS_AUDIO_DOAUDIO  };
-enum TS_Render   {TS_RENDER_INIT,   TS_RENDER_DORENDER};
+enum TS_Joystick : uint8_t {TS_JOYSTICK_INIT, TS_JOYSTICK_SAMPLE};
+enum TS_Buttons  : uint8_t {TS_BUTTONS_INIT,  TS_BUTTONS_SAMPLE };
+enum TS_Update 	 : uint8_t {TS_UPDATE_INIT,   TS_UPDATE_DOUPDATE};
+enum TS_Audio    : uint8_t {TS_AUDIO_INIT,    TS_AUDIO_DOAUDIO  };
+enum TS_Render   : uint8_t {TS_RENDER_INIT,   TS_RENDER_DORENDER};
 
 FORCE_INLINE int Tick_Joystick(int state);
 FORCE_INLINE int Tick_Buttons(int state);
