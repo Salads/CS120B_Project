54f067f81f110975cf48421d26db386e26bc12d0
Salads <dgentry.email@protonmail.com>
2024-05-29 15:06:59 -0700

    Add TextTextures for all numeric digits (1-9)

--- Starship_GalaxyWars/include/TextTexture.h
+++ Starship_GalaxyWars/include/TextTexture.h
@@ -5,7 +5,7 @@
 
 struct TextTexture
 {
-	uint16_t* data;
+	const uint16_t* data;
 	uint16_t  dataSize;
 	uint16_t  width;
 	uint16_t  height;
--- Starship_GalaxyWars/include/TextTextures.h
+++ Starship_GalaxyWars/include/TextTextures.h
@@ -4,3 +4,13 @@
 #include "TextTexture.h"
 
 extern TextTexture kScoreTextTexture;
+extern TextTexture kTextTextureNum1;
+extern TextTexture kTextTextureNum2;
+extern TextTexture kTextTextureNum3;
+extern TextTexture kTextTextureNum4;
+extern TextTexture kTextTextureNum5;
+extern TextTexture kTextTextureNum6;
+extern TextTexture kTextTextureNum7;
+extern TextTexture kTextTextureNum8;
+extern TextTexture kTextTextureNum9;
+
