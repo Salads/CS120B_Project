befc3d3ec9b399807788d8056faabbdcec83ca10
Salads <dgentry.email@protonmail.com>
2024-05-29 15:10:05 -0700

    Add "0" sprite, made "score" label texture PROGMEM

--- Starship_GalaxyWars/include/TextTextures.h
+++ Starship_GalaxyWars/include/TextTextures.h
@@ -4,6 +4,7 @@
 #include "TextTexture.h"
 
 extern TextTexture kScoreTextTexture;
+extern TextTexture kTextTextureNum0;
 extern TextTexture kTextTextureNum1;
 extern TextTexture kTextTextureNum2;
 extern TextTexture kTextTextureNum3;
